0eb5f157715ef887425d896b38561ca955e80b05
Ryan Mullens <programthehacks@gmail.com>
2014-10-16 22:38:17 -0400

    Added listener on server :D

--- Sportana/app.js
+++ Sportana/app.js
@@ -58,3 +58,13 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+
+var server = app.listen(8888, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
