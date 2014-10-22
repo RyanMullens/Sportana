83c4c1eed63a680271cb5b4ed0c229fb98abb8ca
Ryan Mullens <programthehacks@gmail.com>
2014-10-21 23:14:19 -0400

    Set up basic structure/client side routing

---
+++ Sportana/public/javascripts/mainApp.js
@@ -0,0 +1,23 @@
+var app = angular.module('app', ['ui.bootstrap','ngRoute']);
+
+
+console.log("WOOOOOOO");
+
+
+
+  /** * Configure the Routes */ 
+  app.config(['$routeProvider', function ($routeProvider) 
+  {
+ $routeProvider 
+ // Home 
+ .when("/", {templateUrl: "../partials/index.html"}) 
+ // Pages 
+ .when("/friends", {templateUrl: "../partials/viewFriends.html"}) 
+  .when("/game/create", {templateUrl: "../partials/createGame.html"}) 
+   .when("/game/join", {templateUrl: "../partials/joinGame.html"}) 
+    .when("/game/:gameId", {templateUrl: "../partials/viewGame.html"}) 
+     .when("/user/search/:query", {templateUrl: "../partials/searchPlayer.html"}) 
+      .when("/user/:userId", {templateUrl: "../partials/viewProfile.html"}) 
+       .when("/settings", {templateUrl: "../partials/settings.html"}) 
+ // else go home 
+ .otherwise({redirectTo :'/'}); }]);
