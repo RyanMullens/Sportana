b3dcf1f2212b1f3bc168152e50a0e0921860b27a
brandonread <bread@umass.edu>
2014-10-26 05:07:32 -0400

    restyled queue sport selection page, nav and search bar, modified sport selection divs to be selectable

--- Sportana/public/javascripts/controllers.js
+++ Sportana/public/javascripts/controllers.js
@@ -46,18 +46,18 @@ app.controller("QueueController", function(){
 app.controller("SportSelectionController", function($scope){
 
 	var sports = [
-		{icon: "icon", name: "Basketball", 	description: "A game about hoops and nets."},
-		{icon: "icon", name: "Baseball", 		description: "A game about bases."},
-		{icon: "icon", name: "Football", 		description: "A game about hands..."},
-		{icon: "icon", name: "Soccer", 			description: "A game about feet..."},
-		{icon: "icon", name: "Tennis", 			description: "A game about two-us."},
-		{icon: "icon", name: "Billiards", 	description: "A game about sticks and geometry."},
-		{icon: "icon", name: "Swimming", 		description: "Not a game.  Water thingy."},
-		{icon: "icon", name: "Hiking", 			description: "Moving up mountains."},
-		{icon: "icon", name: "Biking", 			description: "Moving up roads."},
+		{icon: "/images/icon_73766.png", name: "Basketball", 	description: "A game about hoops and nets."},
+		{icon: "/images/icon_73766.png", name: "Baseball", 		description: "A game about bases."},
+		{icon: "/images/icon_73766.png", name: "Football", 		description: "A game about hands..."},
+		{icon: "/images/icon_73766.png", name: "Soccer", 			description: "A game about feet..."},
+		{icon: "/images/icon_73766.png", name: "Tennis", 			description: "A game about two-us."},
+		{icon: "/images/icon_73766.png", name: "Billiards", 	description: "A game about sticks and geometry."},
+		{icon: "/images/icon_73766.png", name: "Swimming", 		description: "Not a game.  Water thingy."},
+		{icon: "/images/icon_73766.png", name: "Hiking", 			description: "Moving up mountains."},
+		{icon: "/images/icon_73766.png", name: "Biking", 			description: "Moving up roads."},
 	]; // TODO = $http.get('/api/sports')
 
-	$scope.selectedSports = [];
+	var selectedSports = [];
 
 	this.finish = function() {
 
@@ -66,51 +66,42 @@ app.controller("SportSelectionController", function($scope){
 		// Go to the next page in the queue process
 	}
 
+	this.toggle = function(sport) {
+		
+		var index = selectedSports.indexOf(sport);
+		if(index === -1) {
+			selectedSports.push(sport);
+		} else {
+			selectedSports.splice(index, 1);
+		}
+		return;
+	}
+
+	this.getSelectedSports = function() {
+		return selectedSports;
+	}
+
+	this.hasSelectedSports = function() {
+		return selectedSports.length > 0;
+	}
+
 	this.getSports = function() {
 		return sports;
 	}
 
-	// Currently unused
+	this.isSelected = function(sport) {
+		return (selectedSports.indexOf(sport) != -1) ? true : false;
+	}
+
 	this.addSport = function(sport) {
 		if (selectedSports.indexOf(sport) != -1) return;
 		selectedSports.push(sport);
 	}
 
 	this.removeSport = function(sport) {
-		var index = $scope.selectedSports.indexOf(sport);
+		var index = selectedSports.indexOf(sport);
 		if(index === -1) return;
-		$scope.selectedSports.splice(index, 1);
+		selectedSports.splice(index, 1);
 	}
 
-});
-
-app.directive('checkList', function() {
-  return {
-    scope: {
-      list: '=checkList',
-      value: '@'
-    },
-    link: function(scope, elem, attrs) {
-      var handler = function(setup) {
-        var checked = elem.prop('checked');
-        var index = scope.list.indexOf(scope.value);
-
-        if (checked && index == -1) {
-          if (setup) elem.prop('checked', false);
-          else scope.list.push(scope.value);
-        } else if (!checked && index != -1) {
-          if (setup) elem.prop('checked', true);
-          else scope.list.splice(index, 1);
-        }
-      };
-
-      var setupHandler = handler.bind(null, true);
-      var changeHandler = handler.bind(null, false);
-
-      elem.bind('change', function() {
-        scope.$apply(changeHandler);
-      });
-      scope.$watch('list', setupHandler, true);
-    }
-  };
 });
