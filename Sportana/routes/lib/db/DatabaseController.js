5270de9acb8d8a7f1cbf9527d2084105b2012e78
jboutott <jboutott@umass.edu>
2014-10-28 21:16:18 -0400

    Added database controller

---
+++ Sportana/routes/lib/db/DatabaseController.js
@@ -0,0 +1,65 @@
+var pg = require('pg');
+
+var connString = 'postgres://student:student@localhost/sportana';
+
+function getLogin(callback, login, password) {
+  pg.connect(connString, function (err, client, done) {
+    if (err) {
+      callback(err);
+    }
+    else {
+    	var SQLQuery = "SELECT Users.password FROM Users " +
+                     "WHERE Users.login = $1";
+      client.query({ text : SQLQuery,
+                     values [login]},
+        function (err, result) {
+        // Ends the "transaction":
+        done();
+        // Disconnects from the database:
+        client.end();
+        if (err) {
+          callback(err);
+        }
+        else {
+          if (result.rows["password"] === password) {
+            callback(undefined, true);
+          } else {
+            callback(undefined, false);
+          }
+        }
+      });
+    }
+  });
+}
+
+function getUserByAuth(id, callback) {
+  pg.connect(connString, function (err, client, done) {
+    if (err) {
+      callback(err);
+    }
+    else {
+      var SQLQuery = "SELECT Users.login FROM Users " +
+                     "WHERE Users.auth = $1";
+      client.query({ text : SQLQuery,
+                     values [id]},
+        function (err, result) {
+        // Ends the "transaction":
+        done();
+        // Disconnects from the database:
+        client.end();
+        if (err) {
+          callback(err);
+        }
+        else {
+          if (result.rows["login"] === password) {
+            callback(undefined, result.rows["login"]);
+          } else {
+            callback(undefined, undefined);
+          }
+        }
+      });
+    }
+  });
+}
+
+exports.getLogin = getLogin;
