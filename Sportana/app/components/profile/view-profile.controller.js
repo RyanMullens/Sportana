d71a5dcdf77d02a40efb5d2111534e42160b44a0
Ryan Mullens <programthehacks@gmail.com>
2014-11-09 03:37:43 -0500

    Modified profile view to get info from api, started creating info layout

--- Sportana/app/components/profile/view-profile.controller.js
+++ Sportana/app/components/profile/view-profile.controller.js
@@ -1,4 +1,4 @@
-app.controller("ViewProfileController", function($http, $stateParams){
+app.controller("ViewProfileController", function($http, $stateParams, $scope){
 
 
 	/*
@@ -23,27 +23,28 @@ app.controller("ViewProfileController", function($http, $stateParams){
 	}
 	*/
 
-	this.user = {};
-	this.user.user = "rmullens";
-	this.user.firstName = "Ryan";
-	this.user.lastName = "Mullens";
-	this.user.dateOfBirth = "02-27-1994";
-	this.user.city = "Holliston"
-	this.user.favoriteSports = [{"sportName":"Frisbee","sportImage":"/assets/img/icon_73766.png"}
+	$scope.user = {};
+	/*$scope.user.login = "rmullens";
+	$scope.user.firstname = "Ryan";
+	$scope.user.lastname = "Mullens";
+	$scope.user.dateOfBirth = "02-27-1994";
+	$scope.user.city = "Holliston"
+	$scope.user.favoriteSports = [{"sportName":"Frisbee","sportImage":"/assets/img/icon_73766.png"}
 							,{"sportName":"Soccer","sportImage":"/assets/img/icon_73766.png"}
 							,{"sportName":"Baseball","sportImage":"/assets/img/icon_73766.png"}
 							,{"sportName":"Hockey","sportImage":"/assets/img/icon_73766.png"}];
-	this.user.rating = {"friendliness":5,
+	$scope.user.rating = {"friendliness":5,
 				   "timeliness":4,
 				   "skill": 3};
 
-	this.user.rated = false;
+	$scope.user.rated = false;*/
 
 
-	$http.get('/api/user/' + $stateParams.userId)
+	$http.get('/api/users/' + $stateParams.userId)
 		.success(function(data, status, headers, config)
 		{
-    		//this.user = data;
+			console.log(data);
+    		$scope.user = data;
 		})
 		.error(function(data, status, headers, config) {
     		console.log('There was an error retrieving user profile');
@@ -54,37 +55,55 @@ app.controller("ViewProfileController", function($http, $stateParams){
 
 this.getFullName = function()
 {
-	return this.user.firstName + " " + this.user.lastName;
+	return $scope.user.firstname + " " + $scope.user.lastname;
 }
 
 this.getUsername = function()
 {
-	return this.user.user;
+	return $scope.user.login;
 }
 
 this.getCity = function()
 {
-	return this.user.city;
+	return $scope.user.city;
 }
 
 this.getAge = function()
 {
-	return 21;//return this.user.age;
+	return 25;//return this.user.age;
+}
+
+this.getProfilePicture = function()
+{
+	return $scope.user.profilepicture
 }
 
 this.getFavoriteSports = function()
 {
-	return this.user.favoriteSports;
+	return $scope.user.favoriteSports;
 }
 
-this.getRatings = function()
+/*this.getRatings = function()
 {
-	return this.user.rating;
+	return $scope.user.rating;
+}*/
+
+this.getFriendliness = function()
+{
+	return $scope.user.friendliness;
+}
+this.getSkill = function()
+{
+	return $scope.user.skilllevel;
+}
+this.getTimeliness = function()
+{
+	return $scope.user.timeliness;
 }
 
 this.isRated = function()
 {
-	return this.user.rated;
+	return $scope.user.rated;
 }
 
 this.isSelf = function()
