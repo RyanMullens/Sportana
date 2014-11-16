a4876dff749b669723b07393c30ad65f5e087c48
Alec <arhirsch@umass.edu>
2014-11-16 13:26:22 -0500

    added some mock data for game view

--- Sportana/app/components/games/view-game.controller.js
+++ Sportana/app/components/games/view-game.controller.js
@@ -1,4 +1,25 @@
-app.controller("ViewGameController", function()
+app.controller("ViewGameController", function($http)
 {
-	
+	this.game = {gameid:2, creator: 'myoda', gamedate:'Sunday, November 16',
+	gamestart: '3:00pm', gameend: '5:00pm', location: 'Amherst MA', sport: 'Baseball', 
+	numparticipants: 10, minplayers: 5, maxplayers: 15, reservedspots: 3, minage: 14,
+	maxage:25, ispublic:true};
 });
+
+
+/*
+ creator         | character varying(50)  | not null
+ gameid          | integer                | not null
+ gamedate        | date                   | 
+ gamestart       | time without time zone | 
+ gameend         | time without time zone | 
+ sport           | character varying(50)  | 
+ location        | character varying(100) | 
+ numparticipants | integer                | 
+ minplayers      | integer                | 
+ maxplayers      | integer                | 
+ reservedspots   | integer                | 
+ minage          | integer                | 
+ maxage          | integer                | 
+ ispublic        | boolean              
+ */
