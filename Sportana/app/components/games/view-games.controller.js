e532076c5fb430359c1597ea0c40f2d21612241b
Alec <arhirsch@umass.edu>
2014-11-23 22:15:57 -0500

    started to implement view games with GET requests

--- Sportana/app/components/games/view-games.controller.js
+++ Sportana/app/components/games/view-games.controller.js
@@ -1,23 +1,49 @@
+'use strict';
 app.controller("ViewGamesController", function($http, $scope){
-	/*$http.get('/api/games').success(function(data, status, headers, config) {
-		this.games = data;
-	}).error(function(data, status, headers, config) {
-		console.log('there was an error');
-	});*/
+
 	var games = [
-		{id: 1, creator: 'mscott', sport: 'Football', sportImg: '/assets/img/icon_73766.png',
-		playersJoined: 5,date: 'October 30 2014', 
-		time: '5:30pm', location: 'Amherst', players: ['/assets/img/profile.png','/assets/img/profile.png','/assets/img/profile.png']}
+	{id: 1, creator: 'mscott', sport: 'Football', sportImg: '/assets/img/icon_73766.png',
+	playersJoined: 5,date: 'October 30 2014', 
+	time: '5:30pm', location: 'Amherst', players: ['/assets/img/profile.png','/assets/img/profile.png','/assets/img/profile.png']}
 	];
 
+/*
 	var notifications = [
-		{id: 1, creator: 'jbond', sport: 'Football', sportImg: '/assets/img/icon_73766.png', 
-		invitedBy: 'jbond', playersJoined: 5, playersInvited: 4, 
-		date: 'October 30 2014', time: '5:30pm', location: 'Amherst'},
-		{id: 1, creator: 'ckent', sport: 'Football', sportImg: '/assets/img/icon_73766.png',
-		invitedBy: 'myoda', playersJoined: 5, playersInvited: 4, 
-		date: 'October 30 2014', time: '5:30pm', location: 'Amherst'}
+	{id: 1, creator: 'jbond', sport: 'Football', sportImg: '/assets/img/icon_73766.png', 
+	invitedBy: 'jbond', playersJoined: 5, playersInvited: 4, 
+	date: 'October 30 2014', time: '5:30pm', location: 'Amherst'},
+	{id: 1, creator: 'ckent', sport: 'Football', sportImg: '/assets/img/icon_73766.png',
+	invitedBy: 'myoda', playersJoined: 5, playersInvited: 4, 
+	date: 'October 30 2014', time: '5:30pm', location: 'Amherst'}
 	];
+	*/
+	$scope.notifications = [];
+
+	$http.get('/api/requests/').
+	success(function(data, status, headers, config) {
+		console.log(data.requests);
+		var that = this;
+		for(var i = 0; i < data.requests.length; i++){
+			that.httpGetGame(data.requests[i].gameCreator, data.requests[i].gameID);
+		}
+		
+	}).
+	error(function(data, status, headers, config) {
+		console.log('there was an error');
+	});
+
+	this.httpGetGame = function(gameCreator, gameID){
+		$scope = $scope;
+		$http.get('/api/games/' + gameCreator + '/' + gameID)
+		.success(function(data, status, headers, config){
+			data.sportImg = '/assets/img/sports/' + data.sport.toLowerCase() + '.png';
+			console.log(data);
+			$scope.notification.push(data);
+		})
+		.error(function(data, status, headers, config) {
+			console.log('There was an error retrieving game information');
+		});
+	};
 
 	this.hasGames = function(){
 		return games.length;
@@ -28,30 +54,29 @@ app.controller("ViewGamesController", function($http, $scope){
 	};
 
 	this.acceptGame = function(notification, accepted){
-		/*
-		this.acceptGame = $http.post('/requests/'+notification.id, {confirmed:accepted}).
+		this.acceptGame = $http.post('/requests/'+notification.id, {confirmed:accepted+''}).
 		success(function(data, status, headers, config) {
 	    // this callback will be called asynchronously
 	    // when the response is available
-		}).
+	}).
 		error(function(data, status, headers, config) {
 	    // called asynchronously if an error occurs
 	    // or server returns response with an error status.
-		});
-*/
+	});
+
 		if(accepted){
 			games.push(notification);
-			
+
 		}
-		notifications.splice(notifications.indexOf(notification),1);
+		$scope.notifications.splice($scope.notifications.indexOf(notification),1);
 	};
 
 	this.hasNotifications = function(){
-		return notifications.length;
+		return $scope.notifications.length;
 	};
 
 	this.getNotifications = function(){
-		return notifications;
+		return $scope.notifications;
 	};
 
 });
