6c6d55fb1a638c7dfa3d44c6fd83d158e27a16cf
Ryan Mullens <programthehacks@gmail.com>
2014-12-03 18:06:38 -0500

    Added front end support for editing password in settings

--- Sportana/app/components/settings/settings.controller.js
+++ Sportana/app/components/settings/settings.controller.js
@@ -1,4 +1,46 @@
-app.controller("SettingsController", function()
+app.controller("SettingsController", function($scope,$http)
 {
+	$scope.password = "";
+	$scope.passwordVerify = "";
 
+	$scope.changePass = false;
+	$scope.errorMessage = "";
+
+	$scope.updatePassword = function()
+	{
+		console.log("Update Password!! " + $scope.password + " : " + $scope.passwordVerify);
+
+		$http.post('/api/users/editPassword',{password:$scope.password })
+		.success(function(data, status, headers, config)
+		{
+			if(data.message == "success")
+			{
+				$scope.changePass = true;
+				$scope.password = "";
+				$scope.passwordVerify = "";
+				$scope.changePass = true;
+				$scope.errorMessage = "";
+			}
+			else
+			{
+				$scope.changePass = false;
+				$scope.errorMessage = data.message;
+			}
+		});
+	}
+
+	$scope.arePasswordsSame = function()
+	{
+		return $scope.passwordVerify != "" && $scope.passwordVerify === $scope.password;
+	}
+
+	$scope.wasPassChanged = function()
+	{
+		return $scope.changePass;
+	}
+
+	$scope.wasError = function()
+	{
+		return $scope.errorMessage != "";
+	}
 });
