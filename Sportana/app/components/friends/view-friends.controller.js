f0e6ccfa14e94d4b762fc57e8a3bc4c6660521be
Alec <arhirsch@umass.edu>
2014-12-05 13:59:21 -0500

    added friend requests to friends page

--- Sportana/app/components/friends/view-friends.controller.js
+++ Sportana/app/components/friends/view-friends.controller.js
@@ -1,6 +1,7 @@
 app.controller("ViewFriendsController", function($scope, $http){
 
 	$scope.friends = [];
+	$scope.requests = [];
 
 	$http.get('/api/friends').success(function(data, status, headers, config) {
 		$scope.friends = data.friends;
@@ -10,4 +11,39 @@ app.controller("ViewFriendsController", function($scope, $http){
 	error(function(data, status, headers, config) {
 		console.log('there was an error');
 	});
+
+	$http.get('/api/request/friends').success(function(data, status, headers, config) {
+		$scope.requests = data.requests;
+		console.log(data);
+	}).
+
+	error(function(data, status, headers, config) {
+		console.log('there was an error');
+	});
+
+	$scope.hasRequests = function(){
+		return !!$scope.requests.length;
+	};
+
+	$scope.accept = function(request){
+		$http.post('/api/requests/' + request.id, {confirmed: 'true'})
+		.success(function(data, status, headers, config){
+			$scope.friends.push(request);
+			$scope.request.splice($scope.request.indexOf(reuest),1);
+		})
+		.error(function(data, status, headers, config) {
+			console.log('There was an error with accepting the game');
+		});
+	};
+
+	$scope.decline = function(request){
+		$http.post('/api/requests/' + request.id, {confirmed: 'false'})
+		.success(function(data, status, headers, config){
+			$scope.request.splice($scope.request.indexOf(reuest),1);
+		})
+		.error(function(data, status, headers, config) {
+			console.log('There was an error with accepting the game');
+		});
+	};
+
 });
